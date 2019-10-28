5a00e53df1fdbad310a1b9946a15c4a8594fd1f1
Pat Ugarte <patriciomugarte@gmail.com>
2019-10-28 12:56:12 -0300

    Añadidas otras tres preguntas al prompt de inquirer

--- cliente.js
+++ cliente.js
@@ -16,6 +16,24 @@ let questions = [
         name: "clientPizzaChoice",
         message: "Elegí el gusto de la pizza:",
         choices: ["Mozarella", "Fugazetta", "Napolitana", "Calabresa", "Cuatro Quesos", "Provolone"]
+    },
+    {
+        type: "list",
+        name: "clientPizzaSize",
+        message: "Elegí el tamaño de la pizza:",
+        choices: ["Individual", "Mediana", "Grande"]
+    },
+    {
+        type: "list",
+        name: "clientDrink",
+        message: "Elegí la bebida:",
+        choices: ["Coca-Cola", "Coca-Cola Zero", "Sprite", "Manaos de Uva", "Schweppes Pomelo"]
+    },
+    {
+        type: "checkbox",
+        name: "clientEmpanadasChoice",
+        message: "¿Qué gustos de empanadas querés?",
+        choices: ["Carne Suave","Carne Picante","Jamón y Queso","Queso y Cebolla","Caprese","Ciruela, Queso y Panceta"]
     }
 ];
 
