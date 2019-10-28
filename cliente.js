10d0049683498f34f9bea59179c49418d57b17a2
Pat Ugarte <patriciomugarte@gmail.com>
2019-10-28 12:45:20 -0300

    Ingresadas las primeras tres preguntas al prompt, con la promesa que devuelve simplemente hago un console.log por ahora

--- cliente.js
+++ cliente.js
@@ -1,1 +1,27 @@
 const inquirer = require("inquirer");
+
+let questions = [
+    {
+        type: "input",
+        name: "clientName",
+        message: "Ingresá tu nombre:",
+    },
+    {
+        type: "input",
+        name: "clientPhone",
+        message: "Ingresá tu número de teléfono:",
+    },
+    {
+        type: "rawlist",
+        name: "clientPizzaChoice",
+        message: "Elegí el gusto de la pizza:",
+        choices: ["Mozarella", "Fugazetta", "Napolitana", "Calabresa", "Cuatro Quesos", "Provolone"]
+    }
+];
+
+inquirer
+    .prompt(questions)
+    .then(
+        (answers) => {
+            console.log(answers)
+        });
