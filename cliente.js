abc315ddc00df14f9e4b9758cd500ff9c5d877c1
Pat Ugarte <patriciomugarte@gmail.com>
2019-10-29 01:25:27 -0300

    Añadido el .gitignore al proyecto y quitado del repositorio el directorio node_modules

--- cliente.js
+++ cliente.js
@@ -5,33 +5,33 @@ let questions = [
         type: "input",
         name: "clientName",
         message: "Ingresá tu nombre:",
-        validate: (clientName) => clientName.length >= 3 ? true : "¡Ingrese un nombre válido!"
+        validate: (clientName) => clientName.length >= 3 ? true : "¡Ingrese un nombre válido!",
     },
     {
         type: "input",
         name: "clientPhone",
         message: "Ingresá tu número de teléfono (Sin guiones ni espacios):",
-        validate: (clientPhone) => clientPhone.length >= 8 && !isNaN(clientPhone) ? true : "¡Ingrese un teléfono válido!"
+        validate: (clientPhone) => clientPhone.length >= 8 && !isNaN(clientPhone) ? true : "¡Ingrese un teléfono válido!",
     },
     {
         type: "rawlist",
         name: "clientPizzaChoice",
         message: "Elegí el gusto de la pizza:",
         choices: ["Fugazzetta", "Mozzarella", "Napolitana", "Calabresa", "Cuatro Quesos", "Provolone"],
-        default: 1
+        default: 1,
     },
     {
         type: "list",
         name: "clientPizzaSize",
         message: "Elegí el tamaño de la pizza:",
         choices: ["Individual", "Mediana", "Grande"],
-        default: 2
+        default: 2,
     },
     {
         type: "confirm",
         name: "clientConfirmDrink",
         message: "¿Querés agregar una bebida al pedido?",
-        default: true
+        default: true,
     },
     {
         type: "list",
@@ -39,25 +39,25 @@ let questions = [
         message: "Elegí la bebida:",
         choices: ["Coca-Cola", "Coca-Cola Zero", "Sprite", "Manaos de Uva", "Schweppes Pomelo"],
         when: ({ clientConfirmDrink }) => clientConfirmDrink,
-        default: 0
+        default: 0,
     },
     {
         type: "confirm",
         name: "clientConfirmDelivery",
         message: "¿Querés que te lo enviemos al domicilio?",
-        default: false
+        default: false,
     },
     {
         type: "input",
         name: "clientAdress",
         message: "Ingesá tu dirección:",
         when: ({ clientConfirmDelivery }) => clientConfirmDelivery,
-        validate: (clientAdress) => clientAdress.length > 5 ? true : "¡Ingresá una dirección válida!"
+        validate: (clientAdress) => clientAdress.length > 5 ? true : "¡Ingresá una dirección válida!",
     },
     {
         type: "confirm",
         name: "clientConfirmUsualClient",
-        message: "¿Sos cliente habitual?"
+        message: "¿Sos cliente habitual?",
     },
     {
         type: "checkbox",
@@ -65,7 +65,7 @@ let questions = [
         message: "Por ser cliente habitual te regalamos hasta tres empanadas ¿Qué gustos querés?",
         choices: ["Carne Suave", "Carne Picante", "Jamón y Queso", "Queso y Cebolla", "Caprese", "Ciruela, Queso y Panceta"],
         when: ({ clientConfirmUsualClient }) => clientConfirmUsualClient,
-        validate: (empanadas) => empanadas.length <= 3 ? true : "¡Sólo puede elegir hasta tres empanadas!"
+        validate: (empanadas) => empanadas.length <= 3 ? true : "¡Sólo puede elegir hasta tres empanadas!",
     }
 ];
 
@@ -73,5 +73,5 @@ inquirer
     .prompt(questions)
     .then(
         (answers) => {
-            console.log(answers)
+            console.log(answers);
         });
